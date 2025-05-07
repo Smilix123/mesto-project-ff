93b033e4d5152b5e9ae0b78b072809bd6e6612cb
Smilix123 <smilix@yandex.ru>
2025-05-07 23:51:57 +0400

    v 0.0.3

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,25 @@
 const container = document.querySelector(".content");
 const cardsContainer = container.querySelector(".places__list");
-const addButton = container.querySelector("profile__add-button");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function addCard(nameValue, linkValue) {
+function createCard(cardData) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  return cardElement;
+}
+
+function addCard(cardData, deleteHandler) {
+  const cardElement = createCard(cardData);
   const deleteButton = cardElement.querySelector(".card__delete-button");
-
-  cardElement.querySelector(".card__title").textContent = nameValue;
-  cardElement.querySelector(".card__image").src = linkValue;
+  deleteButton.addEventListener("click", () => deleteHandler(cardElement));
   cardsContainer.append(cardElement);
-
-  deleteButton.addEventListener("click", () => deleteCard(cardElement));
 }
 
 function deleteCard(cardElement) {
   cardElement.remove();
 }
 
-initialCards.forEach((elem) => addCard(elem.name, elem.link));
+initialCards.forEach((cardData) => addCard(cardData, deleteCard));
