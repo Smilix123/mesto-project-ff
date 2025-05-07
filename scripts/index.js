b3cba0cbff85fd1eb288ac38950c4f3a53d4eddd
Smilix123 <smilix@yandex.ru>
2025-05-07 22:57:55 +0400

    v 0.0.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,3 @@
-// @todo: Темплейт карточки
-
-/* const cardTemplate = document.querySelector("#card-template").content;
-const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-const cardsContainer = document.querySelector(".places__list");
-
-for (let i = 0; i < initialCards.length; i++) {
-  console.log(initialCards[i]);
-  cardElement.querySelector(".card__image").src = initialCards[i].link;
-  cardElement.querySelector(".card__title").textContent = initialCards[i].name;
-  cardsContainer.append(cardElement);
-}
-
-function addCard(nameValue, linkValue) {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = songTemplate.querySelector(".card").cloneNode(true);
-
-  cardElement.querySelector(".card__title").textContent = nameValue;
-  cardElement.querySelector(".card__image").src = linkValue;
-  cardsContainer.append(cardElement);
-} */
-
 const container = document.querySelector(".content");
 const cardsContainer = container.querySelector(".places__list");
 const addButton = container.querySelector("profile__add-button");
@@ -27,26 +5,17 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 function addCard(nameValue, linkValue) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
   cardElement.querySelector(".card__title").textContent = nameValue;
   cardElement.querySelector(".card__image").src = linkValue;
   cardsContainer.append(cardElement);
 
-  cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", function (e) {
-      cardElement.remove();
-    });
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
 }
 
-for (let i = 0; i < initialCards.length; i++) {
-  addCard(initialCards[i].name, initialCards[i].link);
+function deleteCard(cardElement) {
+  cardElement.remove();
 }
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((elem) => addCard(elem.name, elem.link));
