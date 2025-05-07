05d7a04ae4b6ec30fa81f36e2496d27bcdd971a0
Smilix123 <smilix@yandex.ru>
2025-05-07 22:31:08 +0400

    v 0.0.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,48 @@
 // @todo: Темплейт карточки
 
+/* const cardTemplate = document.querySelector("#card-template").content;
+const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+const cardsContainer = document.querySelector(".places__list");
+
+for (let i = 0; i < initialCards.length; i++) {
+  console.log(initialCards[i]);
+  cardElement.querySelector(".card__image").src = initialCards[i].link;
+  cardElement.querySelector(".card__title").textContent = initialCards[i].name;
+  cardsContainer.append(cardElement);
+}
+
+function addCard(nameValue, linkValue) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = songTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = nameValue;
+  cardElement.querySelector(".card__image").src = linkValue;
+  cardsContainer.append(cardElement);
+} */
+
+const container = document.querySelector(".content");
+const cardsContainer = container.querySelector(".places__list");
+const addButton = container.querySelector("profile__add-button");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function addCard(nameValue, linkValue) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = nameValue;
+  cardElement.querySelector(".card__image").src = linkValue;
+  cardsContainer.append(cardElement);
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function (e) {
+      cardElement.remove();
+    });
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  addCard(initialCards[i].name, initialCards[i].link);
+}
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
